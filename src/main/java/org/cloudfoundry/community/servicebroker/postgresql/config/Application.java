3f79564d09e920a7e1fd184eed21be5eeb63e2ed
Frank Baalbergen <frank.baalbergen@mendix.com>
2015-04-09 11:18:34 +0200

    cosmetics: also remove copyright of homemade code

--- src/main/java/org/cloudfoundry/community/servicebroker/postgresql/config/Application.java
+++ src/main/java/org/cloudfoundry/community/servicebroker/postgresql/config/Application.java
@@ -22,7 +22,6 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
